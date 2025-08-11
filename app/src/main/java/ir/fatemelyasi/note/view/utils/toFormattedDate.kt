466ee57a4dfaac3c84b5aca5201cfafdaba398e5
Fatemelyasi <fatemelyasii@gmail.com>
2025-08-11 16:48:02 +0430

    Delete old mappers

--- app/src/main/java/ir/fatemelyasi/note/view/utils/toFormattedDate.kt
+++ app/src/main/java/ir/fatemelyasi/note/view/utils/toFormattedDate.kt
@@ -1,6 +1,8 @@
 package ir.fatemelyasi.note.view.utils
 
 fun Long.toFormattedDate(): String {
-    val formatter = java.text.SimpleDateFormat("yyyy/MM/dd", java.util.Locale.getDefault())
+    val formatter = java.text.SimpleDateFormat(
+        "yyyy/MM/dd",
+        java.util.Locale.getDefault())
     return formatter.format(java.util.Date(this))
 }
