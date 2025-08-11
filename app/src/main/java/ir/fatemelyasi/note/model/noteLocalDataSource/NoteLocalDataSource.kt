571408d94c6f9f415cf1500b6760ff5f52b7cb04
Fatemelyasi <fatemelyasii@gmail.com>
2025-08-11 15:53:03 +0430

    ADD NoteWithLabels instead of using mapper Add new Functions for connect between notes and labels

--- app/src/main/java/ir/fatemelyasi/note/model/noteLocalDataSource/NoteLocalDataSource.kt
+++ app/src/main/java/ir/fatemelyasi/note/model/noteLocalDataSource/NoteLocalDataSource.kt
@@ -3,18 +3,13 @@ package ir.fatemelyasi.note.model.noteLocalDataSource
 import ir.fatemelyasi.note.model.local.entity.CrossEntity
 import ir.fatemelyasi.note.model.local.entity.LabelEntity
 import ir.fatemelyasi.note.model.local.entity.NoteEntity
+import ir.fatemelyasi.note.model.local.entity.NoteWithLabels
 import kotlinx.coroutines.flow.Flow
 
 interface NoteLocalDataSource {
 
-    fun getCrossRefs(): Flow<List<CrossEntity>>
-    suspend fun insertCrossRef(cross: CrossEntity)
-    suspend fun insertCrossRefs(crossList: List<CrossEntity>)
-    suspend fun deleteCrossRefsForNote(noteId: Int)
-
-
     fun getAllNotes(): Flow<List<NoteEntity>>
-    fun getNoteById(id: Int): Flow<NoteEntity>?
+    fun getNoteById(id: Long): Flow<NoteEntity>
     suspend fun insertNote(note: NoteEntity) : Long
     suspend fun updateNote(note: NoteEntity)
     suspend fun deleteNote(note: NoteEntity)
@@ -23,10 +18,17 @@ interface NoteLocalDataSource {
     fun getFavoriteNotes(): Flow<List<NoteEntity>>
     suspend fun setFavorite(noteId: Long, isFavorite: Boolean)
 
-    fun getAllLabels(): Flow<List<LabelEntity>>
     fun getByLabelName(label: String): Flow<List<LabelEntity>>
     suspend fun insertLabel(label: LabelEntity)
     suspend fun updateLabel(label: LabelEntity)
     suspend fun deleteLabel(label: LabelEntity)
+
+    fun getNotesWithLabels(): Flow<List<NoteWithLabels>>
+    suspend fun insertCrossRefs(crossList: List<CrossEntity>)
+    suspend fun deleteCrossRefsForNote(noteId: Long)
+    suspend fun getFavoriteNotesWithLabels(): List<NoteWithLabels>
+    suspend fun replaceCrossRefsForNote(noteId: Long, newCrossRefs: List<CrossEntity>)
+    suspend fun updateNoteWithLabels(note: NoteEntity, newCrossRefs: List<CrossEntity>)
+
 
 }
